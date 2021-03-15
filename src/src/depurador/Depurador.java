3992737e064100996ddc5902cb75123de7f036a7
Pedro Rios v <pv123124@gmail.com>
2021-03-15 15:15:30 -0600

    Se agrego un directorio en el que se encuentran los archivos de codigo a depurar y un directorio en el que se guardaran los archivos depurados. Además, se modificaron y agregaron algunas constantes en el código de la clase Depurador.

--- src/src/depurador/Depurador.java
+++ src/src/depurador/Depurador.java
@@ -6,21 +6,23 @@ import java.io.File;
 
 public class Depurador {
     private Queue<String> lines;
-    private final String DIRECTORY;
+    private final String MAIN_PATH;
     private final String FILE_NAME;
+    private final String CODE_PATH = "Codigo\\";
+    private final String DEBUG_PATH = "Depurado\\";
     private final String LANG_TYPE = ".ordev";
     private final String DEBUG_TYPE = ".ordep";
 
     public Depurador(String fileName) {
         lines = new Queue<>();
-        DIRECTORY = System.getProperty("user.dir") + "\\";
+        MAIN_PATH = System.getProperty("user.dir") + "\\";
         this.FILE_NAME = fileName;
         debug();
     }
 
     private void debug() {
-        File f2r = new File(DIRECTORY + FILE_NAME + LANG_TYPE);
-        File f2w = new File(DIRECTORY + FILE_NAME + DEBUG_TYPE);
+        File f2r = new File(MAIN_PATH + CODE_PATH + FILE_NAME + LANG_TYPE);
+        File f2w = new File(MAIN_PATH + DEBUG_PATH + FILE_NAME + DEBUG_TYPE);
         lines = FileHandler.readFile(f2r);
         FileHandler.writeFile(f2w, debuggingCode());
     }
